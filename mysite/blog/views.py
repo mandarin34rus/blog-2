13b2faa2dc430f61f85c874a8206d7aebc9f28ab
mandarin34rus <vaceslavslahov756@gmail.com>
2024-03-10 15:54:59 +0300

    end

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,20 +1,55 @@
+from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Comment
+from django.views.generic import ListView
+
+from .forms import CommentForm
+from django.views.decorators.http import require_POST
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post,
+                                                    'form': form,
+                                                    'comment': comment})
+
+class PostListView(ListView):
+ queryset = Post.published.all()
+ context_object_name = 'posts'
+ paginate_by = 3
+ template_name = 'blog/post/list.html'
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 2)
+    page_number = request.GET.get('page',1)
+    posts =paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(1)
     return render(request, 'blog/post/list.html', {"posts": posts})
 
-
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {"post", post})
-
-
-
-
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, 
+                            status=Post.Status.PUBLISHED,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day,
+                            slug=post)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {"post": post,
+                                                     'comments': comments,
+                                                     'form': form})
 
 def index(request):
     return HttpResponse('<h1>My first http response</h1>')
